d69d413beadd266ae15ce9c54bbae1bdb1dd6b77
hannal <hannal@SPRING01-1>
2017-01-13 18:22:54 -0700

    started

--- CS4150PS1/MrAnaga.cs
+++ CS4150PS1/MrAnaga.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
+
 
 namespace CS4150PS1
 {
@@ -11,28 +14,57 @@ namespace CS4150PS1
         /// <summary>
         /// Available words
         /// </summary>
-        List<String> dictionary;
+        ArrayList words;
         /// <summary>
         /// Words that are not anagrams
         /// </summary>
-        HashSet<String> solutions;
+        HashSet<string> solutions;
         /// <summary>
         /// Words that are anagrams
         /// </summary>
-        HashSet<String> rejected;
+        HashSet<string> rejected;
+
+        /// <summary>
+        /// Sorted version of the word
+        /// </summary>
+        string sortedWord;
 
         static void Main(string[] args)
         {
+            
+
         }
 
+        /// <summary>
+        /// Adds words to the dictionary ArrayList
+        /// </summary>
+        /// <param name="word"></param>
+        private void addWords(string word)
+        {
+            words = new ArrayList();
+            words.Add(word);
+        }
         /// <summary>
         /// Counts the number of words that are not anagrams of other words in the dictionary.
         /// </summary>
         /// <param name="d">A dictionary of words</param>
         /// <returns>The number of words that are not anagrams</returns>
-        public int NotAnagrams(List<String> d)
+        public int NotAnagrams(List<string> d)
         {
-            return 0;
+            foreach(string word in d)
+            {
+                sortedWord = String.Concat(word.OrderBy(c => c));
+                if(solutions.Contains(sortedWord))
+                {
+                    solutions.Remove(sortedWord);
+                    rejected.Add(sortedWord);
+                }
+                else if(!rejected.Contains(sortedWord))
+                {
+                    solutions.Add(sortedWord);
+                }
+            }
+            return solutions.Count;
         }
     }
 }
