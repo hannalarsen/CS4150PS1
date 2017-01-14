47abe1b8b49050ab28372068ebc3b4ac28b2afe6
hannal <hannal@SPRING01-1>
2017-01-13 17:55:41 -0700

    Add project files.

---
+++ CS4150PS1/MrAnaga.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4150PS1
+{
+    class MrAnaga
+    {
+        /// <summary>
+        /// Available words
+        /// </summary>
+        List<String> dictionary;
+        /// <summary>
+        /// Words that are not anagrams
+        /// </summary>
+        HashSet<String> solutions;
+        /// <summary>
+        /// Words that are anagrams
+        /// </summary>
+        HashSet<String> rejected;
+
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Counts the number of words that are not anagrams of other words in the dictionary.
+        /// </summary>
+        /// <param name="d">A dictionary of words</param>
+        /// <returns>The number of words that are not anagrams</returns>
+        public int NotAnagrams(List<String> d)
+        {
+            return 0;
+        }
+    }
+}
