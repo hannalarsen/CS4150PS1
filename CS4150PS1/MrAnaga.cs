9d9b8686da11e28fc79343b2ddee9ed3ba1ff283
Hanna Larsen <hanna.c.larsen@gmail.com>
2017-01-15 17:36:29 -0700

    read and write from console

--- CS4150PS1/MrAnaga.cs
+++ CS4150PS1/MrAnaga.cs
@@ -35,69 +35,68 @@ namespace CS4150PS1
         public static void Main (string[] args)
         {
             MrAnaga m = new MrAnaga();
-            string file = "";
-            Console.Write("Enter file path: ");
-            file = Console.ReadLine();
-            Console.WriteLine(m.NotAnagrams(file).ToString());
+            m.AddWords();
+        }
+
+        /// <summary>
+        /// Method that adds words from standard input into the dictionary Arraylist
+        /// </summary>
+        private void AddWords()
+        {
+            words = new ArrayList();
+            string word = "";
+            do
+            {
+                word = Console.ReadLine();
+                if (word.Length > 0 && (word != null))
+                {
+                    words.Add(word);
+                }
+            }
+            while (word.Length > 0 && word != null);
+            NotAnagrams2(words);
         }
         /// <summary>
         /// Counts the number of words that are not anagrams of other words in the dictionary.
         /// </summary>
         /// <param name="filePath">Path of the file containing words to be sorted</param>
         /// <returns>The number of words that are not anagrams</returns>
-        public int NotAnagrams(string filePath)
+        public void NotAnagrams2(ArrayList w)
         {
-            // Creates new instances of the collections of words
-            words = new ArrayList();
             solutions = new HashSet<string>();
             rejected = new HashSet<string>();
-            // Trys to open the file
-            try
-            {
-                using (StreamReader sr = File.OpenText(filePath))
-                {
-                    string line = "";
-                    // Adds the words to the Arraylist of words
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        words.Add(line);
-                    }
-                }
-            }
-            // Catches any exceptions and throws the exception
-            catch (Exception e)
-            {
-                throw e;
-            }
 
-            // If there are no words in the file
+            // If there are no words in the dictionary
             if (words.Count == 0)
             {
-                return 0;
+               Console.WriteLine(0.ToString());
+                return;
             }
-                foreach (string word in words)
-                {
+            foreach (string word in words)
+            {
                 // Sorts the word alphabetically
-                    sortedWord = String.Concat(word.OrderBy(c => c));
+                sortedWord = String.Concat(word.OrderBy(c => c));
                 // Checks to see if it already contained in solutions.  If yes, adds to rejected.
-                    if (solutions.Contains(sortedWord))
-                    {
-                        solutions.Remove(sortedWord);
-                        rejected.Add(sortedWord);
-                    }
-                    // Otherwise adds word to solutions
-                    else if (!rejected.Contains(sortedWord))
-                    {
-                        solutions.Add(sortedWord);
-                    }
+                if (solutions.Contains(sortedWord))
+                {
+                    solutions.Remove(sortedWord);
+                    rejected.Add(sortedWord);
                 }
+                // Otherwise adds word to solutions
+                else if (!rejected.Contains(sortedWord))
+                {
+                    solutions.Add(sortedWord);
+                }
+            }
 
             if (solutions.Count == 0)
             {
-                return 0;
+                Console.WriteLine(0.ToString());
+                return;
             }
-                // Returns the number of unique words (minus one to account for the first row of numbers)
-            return solutions.Count - 1;
+            int total = solutions.Count - 1;
+            // Returns the number of unique words (minus one to account for the first row of numbers)
+            Console.WriteLine(total.ToString());
         }
     }
 }
